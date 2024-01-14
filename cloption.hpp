c799711ecb35a2cb6d599dc49a32f606f67fad7f
kowat69 <kowat20060609@gmail.com>
2024-01-14 22:07:59 +0900

    windows環境終了

--- cloption.hpp
+++ cloption.hpp
@@ -37,7 +37,7 @@ public:
             std::filesystem::path p = arg;
             infile.push_back(p.string());
             outasm.push_back(p.replace_extension(".asm").string());
-            outobj.push_back(p.parent_path().string() + std::string("/.tmp/") + p.filename().replace_extension(".obj").string());
+            outobj.push_back(p.parent_path().string() + std::string("./.tmp/") + p.filename().replace_extension(".obj").string());
         }
         if(outexe.size() == 0){
             outexe = std::filesystem::path(infile[0]).replace_extension(".exe").string();
@@ -54,7 +54,37 @@ public:
         std::cout << outexe << std::endl;
 
     }
+    inline void NASM(){
+        for(std::string nasm : getNASM()){
+            system(nasm.c_str());
+        }
+    }
+    inline void LLD(){
+        system(getLLD().c_str());
+    }
 private:
+    inline std::vector<std::string> getNASM(){
+        std::vector<std::string> ret;
+        std::stringstream output;
+        for(int i = 0; i < outasm.size(); i++ ){
+            output << nasm << ' ';
+            output << outasm[i] << ' ';
+            output << nasm_opt << " -o " << outobj[i];
+            ret.push_back(output.str());
+            output.clear();
+        }
+        return ret;
+    }
+    inline std::string getLLD(){
+        std::stringstream output;
+        output << lld << ' ';
+        for(std::string obj : outobj){
+            output << obj << ' ';
+        }
+        output << "/OUT:" << outexe << ' ';
+        output << lld_opt;
+        return output.str();
+    }
     inline std::optional<std::string> opEqual(const std::string::const_iterator first, const std::vector<std::string> strs){
         for(std::string str : strs){
             for(int i = 0; i < str.size(); i++){
@@ -67,7 +97,10 @@ private:
     std::vector<std::string> infile;
     std::vector<std::string> outasm;
     std::vector<std::string> outobj;
-    std::string linker = "lld-link";
+    std::string lld = "lld-link";
+    std::string lld_opt = "/ENTRY:main /SUBSYSTEM:CONSOLE /MACHINE:X64";
+    std::string nasm = "nasm";
+    std::string nasm_opt = "-fwin64";
     std::string outexe = "";
 
 };
--- vic.cpp
+++ vic.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]){
         std::cerr << "vic <input.vi>" << std::endl;
         return EXIT_FAILURE;
     }
-    std::chrono::system_clock::time_point  start, end, end2; // 型は auto で可
+    std::chrono::system_clock::time_point  start, end, _lld; // 型は auto で可
     start = std::chrono::system_clock::now(); // 計測開始時間
 
 
@@ -54,23 +54,18 @@ int main(int argc, char* argv[]){
     }
     end = std::chrono::system_clock::now();  // 計測終了時間
 //
-    std::string nasm_option = "nasm -f win64 " + out_asm.string();
-    system(nasm_option.c_str());
+    Clopt clopt(argc, argv);
+    
+    clopt.NASM();
+    
     std::chrono::system_clock::time_point _nasm = std::chrono::system_clock::now();
 
-    std::string linker = "lld-link ";
-    std::string link_infile = out_obj.string();
-    std::string link_outfile = std::string("/OUT:") + out_exe.string();
-    std::string linker_option = 
-        " /ENTRY:main /SUBSYSTEM:CONSOLE /MACHINE:X64"
-        " ";
-    system((linker + link_infile + link_outfile + linker_option).c_str());
+    clopt.LLD();
 //    
-    end2 = std::chrono::system_clock::now();
-    std::cout << "compile: "<<std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count() << "ms" << std::endl; //処理に要した時間をミリ秒に変換
-    std::cout << "link: "<<std::chrono::duration_cast<std::chrono::milliseconds>(end2-end).count() << "ms" << std::endl; //処理に要した時間をミリ秒に変換
+    _lld = std::chrono::system_clock::now();
+    std::cout << "compile: "<<std::chrono::duration_cast<std::chrono::milliseconds>(_lld-start).count() << "ms" << std::endl; //処理に要した時間をミリ秒に変換
     std::cout << "nasm: "<<std::chrono::duration_cast<std::chrono::milliseconds>(_nasm-end).count() << "ms" << std::endl; //処理に要した時間をミリ秒に変換
+    std::cout << "link: "<<std::chrono::duration_cast<std::chrono::milliseconds>(_lld-_nasm).count() << "ms" << std::endl; //処理に要した時間をミリ秒に変換
 
-    Clopt clopt(argc, argv);
     return EXIT_SUCCESS;
 }
